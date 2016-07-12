21b08d41bd058a0777c7a2c76c18014cd5fd1b9f
John Karahalis <john.karahalis@gmail.com>
2016-07-12 16:04:35 -0400

    Add some basic styles
    Credit where credit's due; these styles are basically just a copy of
    Davor's styles for SHIELDDash.

--- distributionviewer/core/static/js/app/components/home.js
+++ distributionviewer/core/static/js/app/components/home.js
@@ -4,9 +4,6 @@ import ExampleChartContainer from './containers/example-chart-container';
 
 export default function(props) {
   return (
-    <div>
-      <h1>Distribution Viewer</h1>
-      <ExampleChartContainer />
-    </div>
+    <ExampleChartContainer />
   );
 }
--- distributionviewer/core/static/js/app/components/layouts/main-layout.js
+++ distributionviewer/core/static/js/app/components/layouts/main-layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default function(props) {
   return (
     <div className="global-wrapper">
-      <header className="primary-header"></header>
+      <header className="primary-header">
+        <h1><Link className="primary-header-link" to="/">Firefox Distribution Viewer</Link></h1>
+      </header>
       <main>
         {props.children}
       </main>
