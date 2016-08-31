c20671200c460c0dedfc2b69a9173040c9ad579d
John Karahalis <john.karahalis@gmail.com>
2016-08-31 13:20:36 -0400

    Add placeholder 404 page

---
+++ distributionviewer/core/static/js/app/components/views/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export class NotFound extends React.Component {
+  render() {
+    return (
+      <main>
+        <h2>Not found</h2>
+      </main>
+    );
+  }
+}
--- distributionviewer/core/static/js/app/router.js
+++ distributionviewer/core/static/js/app/router.js
@@ -7,12 +7,14 @@ import MainLayout from './components/layouts/main-layout';
 // Pages
 import Home from './components/home';
 import ChartDetailContainer from './components/containers/chart-detail-container';
+import { NotFoundContainer } from './components/containers/not-found-container';
 
 export default (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route path="/" component={Home} />
       <Route path="/chart/:chartId" component={ChartDetailContainer} />
+      <Route path="*" component={NotFoundContainer} />
     </Route>
   </Router>
 );
