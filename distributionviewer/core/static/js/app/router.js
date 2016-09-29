49522c30ff9fc3d0527a7e2bb83f2e6701ccbfad
Davor Spasovski <davor@spasovski.net>
2016-09-29 17:46:53 -0400

    rm unnecessary NotFoundContainer

--- distributionviewer/core/static/js/app/router.js
+++ distributionviewer/core/static/js/app/router.js
@@ -7,14 +7,14 @@ import MainLayout from './components/layouts/main-layout';
 // Pages
 import Home from './components/home';
 import ChartDetailContainer from './components/containers/chart-detail-container';
-import NotFoundContainer from './components/containers/not-found-container';
+import NotFound from './components/views/not-found';
 
 export default (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route path="/" component={Home} />
       <Route path="/chart/:chartId" component={ChartDetailContainer} />
-      <Route path="*" component={NotFoundContainer} />
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
