53e2c557ca88cd5fa4dfe8e30c6a157050c1eeb6
Rob Hudson <robhudson@mozilla.com>
2016-11-04 11:04:45 -0700

    Add batch import of metric data to admin (fixes #143)

--- distributionviewer/api/admin.py
+++ distributionviewer/api/admin.py
@@ -1,10 +1,37 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import Textarea
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 
-from .models import Metric
+from .forms import CSVForm
 
 
-@admin.register(Metric)
 class MetricAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'source_name', 'type', 'tooltip', 'description']
     list_editable = list_display
     list_filter = ['type']
+
+    formfield_overrides = {
+        models.TextField: {
+            'widget': Textarea(attrs={'rows': 3, 'columns': 40})
+        }
+    }
+
+    def get_urls(self):
+        urls = super(MetricAdmin, self).get_urls()
+        return [
+            url(r'^import/$', self.admin_site.admin_view(self.batch_import)),
+        ] + urls
+
+    def batch_import(self, request):
+        if request.method == 'POST':
+            form = CSVForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('admin:api_metric_changelist')
+        else:
+            form = CSVForm()
+            return TemplateResponse(request, 'admin/api/metric_import.html',
+                                    {'form': form})
--- distributionviewer/urls.py
+++ distributionviewer/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 from . import views
+from .admin import admin_site
 from .api.views import login_view, metric, metrics, verify_google_token
 
 
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^metrics/$', metrics, name='metrics'),
     url(r'^metric/(?P<metric_id>\d+)/$', metric, name='metric'),
     url(r'^admin/login/$', login_view),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin_site.urls)),
     url(r'^accounts/login/$', login_view, name='login'),
     url(r'^accounts/logout/$', auth_views.logout_then_login, name='logout'),
     url(r'^verify_google_token/$', verify_google_token,
