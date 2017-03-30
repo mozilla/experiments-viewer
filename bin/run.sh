1f960e2a317fed0795a00e8986c5a97fef7ac4e4
Rob Hudson <robhudson@mozilla.com>
2017-03-30 11:46:42 -0400

    Wait for db
    This is still required. The `depends_on` only controls start order.

--- bin/run.sh
+++ bin/run.sh
@@ -11,6 +11,7 @@ run_front_end_tests() {
 case $1 in
   dev)
     # Wait for database to get started.
+    /bin/sleep 5
     ./manage.py migrate --noinput
     # Try running this in a loop, so that the whole container
     # doesn't exit when runserver reloads and hits an error.
