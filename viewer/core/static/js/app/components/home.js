0ef3b34fc2d43811ee3d13c6fb2caee28be3a16c
Davor Spasovski <davor@spasovski.net>
2017-04-03 16:11:47 -0400

    rm introduction, chart menu

--- viewer/core/static/js/app/components/home.js
+++ viewer/core/static/js/app/components/home.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import ChartListContainer from './containers/chart-list-container';
-import ChartMenuContainer from './containers/chart-menu-container';
-import Introduction from './views/introduction';
 
 
 export default function(props) {
   return (
     <div className="home">
-      <ChartMenuContainer {...props} />
       <main>
-        <Introduction />
         <ChartListContainer {...props} />
       </main>
     </div>
