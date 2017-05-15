2d21ca8583d37d9c5bd59a9a9b0e6041eae58973
John Karahalis <john.karahalis@gmail.com>
2017-05-15 15:30:23 -0400

    Issue #112: Move data- attributes to li in legend

--- viewer/static/js/app/components/views/legend.js
+++ viewer/static/js/app/components/views/legend.js
@@ -7,8 +7,8 @@ export default function(props) {
       <ul>
         {props.sortedPopulationsToShow.map(populationName => {
           return (
-            <li key={populationName}>
-              <svg className="example-line" data-population={populationName} data-population-id={props.populationIds[populationName]} width="50" height="5">
+            <li key={populationName} data-population={populationName} data-population-id={props.populationIds[populationName]}>
+              <svg className="example-line" width="50" height="5">
                 <line x1="0" y1="5" x2="50" y2="5" strokeWidth="5" />
               </svg>
               <span className="name">
