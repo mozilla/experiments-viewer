5b6312456d0168fc4de9c9feebc853ff3f27d213
John Karahalis <john.karahalis@gmail.com>
2017-05-17 16:50:26 -0400

    Issue #103: Fix issue where some charts don't load

--- viewer/static/js/app/components/views/chart.js
+++ viewer/static/js/app/components/views/chart.js
@@ -7,125 +7,104 @@ import ChartHoverContainer from '../containers/chart-hover-container';
 import ChartFocus from './chart-focus';
 
 
-export default class extends React.Component {
-  /**
-   * sortedPopulationsToShow often updates before populationData does. That is,
-   * sortedPopulationsToShow often names populations that do not yet have
-   * corresponding data in populationData.
-   *
-   * This component can't exactly do much when that happens. It can't exactly
-   * render data that it doesn't have yet. So it's better off not re-rendering
-   * at all. As soon as populationData is updated to have data for all of the
-   * populations named in sortedPopulationsToShow, this test will pass and the
-   * component will re-render.
-   */
-  shouldComponentUpdate(nextProps) {
-    let shouldUpdate = true;
-
-    for (let i = 0; i < nextProps.sortedPopulationsToShow.length; i++) {
-      const populationName = nextProps.sortedPopulationsToShow[i];
-      if (!nextProps.populationData.hasOwnProperty(populationName)) {
-        shouldUpdate = false;
-        break;
-      }
-    }
-
-    return shouldUpdate;
-  }
-
-  render() {
-    if (this.props.noData) {
-      return (
-        <div className={`chart chart-${this.props.metricId} no-data`}>
-          <span className="warning">No data</span>
-          <span>(try selecting different cohorts)</span>
+export default function(props) {
+  if (props.noData) {
+    return (
+      <div className={`chart chart-${props.metricId} no-data`}>
+        <span className="warning">No data</span>
+        <span>(try selecting different cohorts)</span>
+      </div>
+    );
+  } else if (props.isFetching) {
+    return (
+      <div className={`chart is-fetching chart-${props.metricId}`}>
+        <Fetching />
+      </div>
+    );
+  } else {
+    return (
+      <div className={`chart chart-${props.metricId}`}>
+        <div className={props.tooltip ? 'tooltip-wrapper' : ''}>
+          <h2 className={`chart-list-name ${props.tooltip ? 'tooltip-hover-target' : ''}`}>{props.name}</h2>
+          {props.tooltip ? props.tooltip : ''}
         </div>
-      );
-    } else if (this.props.isFetching) {
-      return (
-        <div className={`chart is-fetching chart-${this.props.metricId}`}>
-          <Fetching />
-        </div>
-      );
-    } else {
-      const reverseSortedPopulationsToShow = this.props.sortedPopulationsToShow.slice(0).reverse();
+        <svg width={props.size.width} height={props.size.height}>
+          <g transform={props.size.transform}>
+            <ChartAxisContainer
+              metricId={props.metricId}
+              metricType={props.metricType}
+              scale={props.xScale}
+              axisType="x"
+              refLabels={props.refLabels}
+              size={props.size.innerHeight}
+            />
+            <ChartAxisContainer
+              metricId={props.metricId}
+              scale={props.yScale}
+              axisType="y"
+              refLabels={props.refLabels}
+              size={props.size.innerWidth}
+            />
+            <g className="populations">
+              {/*
+              sortedPopulationsToShow is an array of population names sorted by
+              their visual importance. The first name in that array should be
+              shown first in lists, for example, and the last name in the array
+              should be shown last.
 
-      return (
-        <div className={`chart chart-${this.props.metricId}`}>
-          <div className={this.props.tooltip ? 'tooltip-wrapper' : ''}>
-            <h2 className={`chart-list-name ${this.props.tooltip ? 'tooltip-hover-target' : ''}`}>{this.props.name}</h2>
-            {this.props.tooltip ? this.props.tooltip : ''}
-          </div>
-          <svg width={this.props.size.width} height={this.props.size.height}>
-            <g transform={this.props.size.transform}>
-              <ChartAxisContainer
-                metricId={this.props.metricId}
-                metricType={this.props.metricType}
-                scale={this.props.xScale}
-                axisType="x"
-                refLabels={this.props.refLabels}
-                size={this.props.size.innerHeight}
-              />
-              <ChartAxisContainer
-                metricId={this.props.metricId}
-                scale={this.props.yScale}
-                axisType="y"
-                refLabels={this.props.refLabels}
-                size={this.props.size.innerWidth}
-              />
-              <g className="populations">
-                {/*
-                In SVG, the element that appears last in source order paints above
-                everything it overlaps. It has the greatest "z-index", so to
-                speak.
+              In SVG, the element that appears last in source order paints above
+              everything it overlaps. It has the greatest "z-index", so to
+              speak.
 
-                sortedPopulationsToShow is an array of population names sorted
-                by their visual importance. The first name in that array
-                should be shown first in lists, for example, and the last name
-                in the array should be shown last.
+              So we need to render these populations in reverse order.
+              reduceRight() is used to achive this. It's essentially acting like
+              a backwards map() here. The line for the last population according
+              to the original sort order is painted first, then the line for the
+              second-to-last population, and so on. At the end of this process,
+              the first population in the original sort order is painted last
+              and has the highest z-index. Hooray!
 
-                As a result, we need to render these populations in reverse order.
-                The line for the last population according to the original sort
-                order is painted first, then the line for the second-to-last
-                population, and so on. At the end of this process, the first
-                population in the original sort order is painted last and has the
-                highest z-index. Hooray!
+              It goes without saying, but we don't want to iterate over
+              populationData, even though we ultimately use the data from that
+              object, because object ordering is indeterminate.
+              */}
+              {props.sortedPopulationsToShow.reduceRight((previousPopulationName, currentPopulationName) => {
+                // sortedPopulationsToShow updates before populationData does.
+                // So it sometimes contains populations that don't have any
+                // corresponding data yet.
+                //
+                // If there is no data for this population, don't attempt to
+                // render it.
+                if (!props.populationData[currentPopulationName]) return null;
 
-                It goes without saying at this point, but we don't want to
-                iterate over populationData, even though we ultimately use the
-                data from that object, because object ordering is
-                indeterminate.
-                */}
-                {reverseSortedPopulationsToShow.map(populationName => {
-                  return (
-                    <g key={this.props.metricId + populationName} className="population" data-population={populationName} data-population-id={this.props.populationIds[populationName]}>
-                      <ChartLineContainer
-                        populationName={populationName}
-                        metricId={this.props.metricId}
-                        xScale={this.props.xScale}
-                        yScale={this.props.yScale}
-                        data={this.props.populationData[populationName][this.props.activeDatasetName]}
-                      />
-                      <ChartFocus />
-                    </g>
-                  );
-                })}
-              </g>
-              <ChartHoverContainer
-                metricId={this.props.metricId}
-                size={this.props.size}
-                xScale={this.props.xScale}
-                yScale={this.props.yScale}
-                populations={this.props.populationData}
-                activeDatasetName={this.props.activeDatasetName}
-                hoverString={this.props.hoverString}
-                refLabels={this.props.refLabels}
-                metricType={this.props.metricType}
-              />
+                return (
+                  <g key={props.metricId + currentPopulationName} className="population" data-population={currentPopulationName} data-population-id={props.populationIds[currentPopulationName]}>
+                    <ChartLineContainer
+                      populationName={currentPopulationName}
+                      metricId={props.metricId}
+                      xScale={props.xScale}
+                      yScale={props.yScale}
+                      data={props.populationData[currentPopulationName][props.activeDatasetName]}
+                    />
+                    <ChartFocus />
+                  </g>
+                );
+              })}
             </g>
-          </svg>
-        </div>
-      );
-    }
+            <ChartHoverContainer
+              metricId={props.metricId}
+              size={props.size}
+              xScale={props.xScale}
+              yScale={props.yScale}
+              populations={props.populationData}
+              activeDatasetName={props.activeDatasetName}
+              hoverString={props.hoverString}
+              refLabels={props.refLabels}
+              metricType={props.metricType}
+            />
+          </g>
+        </svg>
+      </div>
+    );
   }
 }
