41d38d640df5d2d6154b8763422c483cc86418a9
John Karahalis <john.karahalis@gmail.com>
2017-05-25 17:40:15 -0400

    Fix #15: Add Google Analytics tracking

--- viewer/static/js/app/router.js
+++ viewer/static/js/app/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
+import ReactGA from 'react-ga';
 
 // Layouts
 import MainLayout from './components/layouts/main-layout';
@@ -12,8 +13,16 @@ import NotFound from './components/views/not-found';
 import PermissionDenied from './components/views/permission-denied';
 
 
+// Copied from:
+// https://github.com/react-ga/react-ga#with-npm
+ReactGA.initialize(process.env.TRACKING_ID);
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 export default (
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     <Route component={AppContainer}>
       <Route component={MainLayout}>
         <Route path="/" component={Home} />
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,6 +5,7 @@ const plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'TRACKING_ID': JSON.stringify(process.env.TRACKING_ID),
     }
   })
 ];
